e70d102c5cb14aef91321de37bd550db96359813
DESKTOP-T69JMMQ\신헌우 <shinhunwoo1122@gmail.com>
2025-09-22 17:11:15 +0900

    commit

--- src/main/java/com/project/testProject/controller/TestController.java
+++ src/main/java/com/project/testProject/controller/TestController.java
@@ -3,7 +3,6 @@ package com.project.testProject.controller;
 import com.project.testProject.common.BaseResponse;
 import com.project.testProject.common.CommonResponse;
 import com.project.testProject.common.MetaData;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +13,6 @@ public class TestController {
     @GetMapping("/test")
     public ResponseEntity<BaseResponse> test(){
 
-        var name = "신헌우";
 
         return ResponseEntity.ok(new CommonResponse<>(MetaData.builder().result(true).message("hello").build(), null));
     }
