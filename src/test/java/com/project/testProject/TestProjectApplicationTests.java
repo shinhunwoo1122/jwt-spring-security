5605a1853c19bb693c7c8cdb3acdfbb864dc1470
DESKTOP-T69JMMQ\신헌우 <shinhunwoo1122@gmail.com>
2025-09-22 16:22:36 +0900

    최초올리기

---
+++ src/test/java/com/project/testProject/TestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.testProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
